7af44b778c724060f20e1a25e2d456a58313f642
Clément DROUET <drouet.clement02@gmail.com>
2026-05-04 00:39:30 +0200

    add notes

--- 05-calculation/main.asm
+++ 05-calculation/main.asm
@@ -5,7 +5,7 @@ global _start
 
 ; since we will overwrite the value later without setting a default value, we declare add_result in bss section
 section .bss
-    add_result resb 1 ; Sadly because it's bss and we can't set a default value, we can't use '10' for a newline character
+    add_result resb 1 ; Since it's bss section we can't set a default value, so we can't use '10' for a newline character
                       ; we'll create a 'newline' label in data section to solve this problem.
 
 section .data
@@ -17,7 +17,7 @@ _start:
     add_numbers:
         mov rbx, 5 ; rbx = 5
         add rbx, 4 ; rbx = rbx + 4 = 9
-        add rbx, '0' ; convert the number to ASCII (terminal only displays ASCII characters)
+        add rbx, '0' ; convert the number to ASCII (terminal only displays ASCII characters) -> check notes below for more details.
         mov [add_result], bl ; we use bl because it's the lower 8 bits of rbx (1 byte long, perfect for add_result)
 
     ; print the result in the terminal (see 02-hello/main.asm for more details)
@@ -41,3 +41,10 @@ _start:
         mov rax, 60
         mov rdi, 0
         syscall
+
+; Notes:
+; add rbx, '0' converts rbx value into ASCII. It's works because numbers in ASCII are consecutive and start at 48. (0=48, 1=49 etc.)
+; so if we add 48 (ASCII of '0') to our number, we will get it's ASCII value.
+
+; Oh, little bit of humility, here's the exact python version of this program:
+; print(5 + 4)
