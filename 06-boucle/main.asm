0c1220c96f7dfbe6f384fa1cfa3caa3695ae49dd
Clément DROUET <drouet.clement02@gmail.com>
2026-05-04 08:51:12 +0200

    better loop

--- 06-boucle/main.asm
+++ 06-boucle/main.asm
@@ -8,17 +8,20 @@ section .text
 
 _start:
     mov rbx, 1
+
+    ; while rbx <= 5, print a star and increment rbx by 1
     loop_start:
-        mov rdi, 1
-        mov rax, 1
-        mov rsi, star
-        mov rdx, star_length
-        syscall
+        print_star:
+            mov rdi, 1
+            mov rax, 1
+            mov rsi, star
+            mov rdx, star_length
+            syscall
 
         inc rbx ; increment rbx by 1
 
-        cmp rbx, 5
-        jle loop_start
+        cmp rbx, 5 ; compare rbx with 5
+        jle loop_start ; jump to loop_start if rbx <= 5
         
     exit:
         mov rax, 60
