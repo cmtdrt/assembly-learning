523d2054646a3c113b3fcbf3f9798fbd4b6b355c
Clément DROUET <drouet.clement02@gmail.com>
2026-05-04 09:32:52 +0200

    for is working

--- 06-boucle/main.asm
+++ 06-boucle/main.asm
@@ -1,29 +1,43 @@
+; This program prints a pyramid of stars '*'
 global _start
 
 section .data
-    star db "*", 10
-    star_length: equ $ - star
+    star db "*"
+    newline db 10 ; in ASCII, 10 is the newline character
 
 section .text
 
 _start:
     mov rbx, 1
-
-    ; while rbx <= 5, print a star and increment rbx by 1
-    loop_start:
-        print_star:
-            mov rdi, 1
+    loop_line:
+        mov r12, 0
+        loop_star:
+            cmp r12, rbx
+            jge print_newline
+            
+            ; print a star
             mov rax, 1
+            mov rdi, 1
             mov rsi, star
-            mov rdx, star_length
+            mov rdx, 1 ; star is always 1 byte long so we can hardcode it's length
+            syscall
+
+            inc r12 ; increment r12 by 1
+            jmp loop_star
+        
+        ; print a newline character
+        print_newline:
+            mov rax, 1
+            mov rdi, 1
+            mov rsi, newline
+            mov rdx, 1 ; newline is always 1 byte long so we can hardcode it's length
             syscall
 
         inc rbx ; increment rbx by 1
-
         cmp rbx, 5 ; compare rbx with 5
-        jle loop_start ; jump to loop_start if rbx <= 5
-        
-    exit:
-        mov rax, 60
-        mov rdi, 0
-        syscall
+        jle loop_line ; jump to loop_start if rbx <= 5
+
+    ; exit the program 
+    mov rax, 60
+    xor rdi, rdi ; apparently it's the same as mov rdi, 0 but it's more efficient to set a register to 0 like this
+    syscall
